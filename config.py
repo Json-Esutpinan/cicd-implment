2961fc07a1435a73cb3b9484c8004ce896e83381
Json Estupinan <jeison.code@hotmail.com>
2025-05-23 16:26:41 -0500

    Add: Requirements

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 import os
 
 class Config:
-    # Azure App Service inyecta estas variables en el entorno de la aplicación
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_NAME = os.getenv('DB_NAME')
+    @property
+    def SQLALCHEMY_DATABASE_URI(self):
+        DB_USER = os.getenv('DB_USER')
+        DB_PASSWORD = os.getenv('DB_PASSWORD')
+        DB_HOST = os.getenv('DB_HOST')
+        DB_NAME = os.getenv('DB_NAME')
+        return f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
-    # Formato de la cadena de conexión para MySQL con SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
